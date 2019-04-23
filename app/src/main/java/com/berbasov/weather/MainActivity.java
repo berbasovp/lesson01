a01227ba0a492fc931a3a858254ea956552bbcc9
pavel <director@depato.ru>
2019-04-23 10:39:42 +0300

    Добавил логирование

--- app/src/main/java/com/berbasov/weather/MainActivity.java
+++ app/src/main/java/com/berbasov/weather/MainActivity.java
@@ -2,12 +2,14 @@ package com.berbasov.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String TAG = "Жизненный цикл";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate()");
     }
 }
